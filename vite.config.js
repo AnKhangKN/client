58f2622758f6155ae20369531dd46958acd0ef46
AnKhang <minecraftcopyright1302@gmail.com>
2025-12-10 13:21:35 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ const __dirname = path.dirname(__filename);
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   assetsInclude: ["**/*.docx", "**/*.pptx", "**/*.xlsx", "**/*.pdf"],
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), // @ trỏ đến thư mục src
